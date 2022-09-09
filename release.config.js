b1500b26a2c4f4f50c7b520eecca36181c0b07d3
Xborg908 <decentragit@gmail.com>
2022-09-09 15:16:13 +0300

    add conventional versioning

---
+++ release.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    branches: "master",
+    repositoryUrl:  "https://github.com/georges-hayek/nodeApp",
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        ["@semantic-release/github", {
+          "assets": [
+            {"path": "dist/asset.min.css", "label": "CSS distribution"},
+            {"path": "dist/asset.min.js", "label": "JS distribution"}
+          ]
+        }],
+      ]
+
+}
