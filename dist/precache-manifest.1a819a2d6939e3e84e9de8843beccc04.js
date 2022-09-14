9d0aaba447c441f3a1a1d38410a6f3c1ac96a6ea
Xborg908 <decentragit@gmail.com>
2022-09-14 16:50:30 +0300

    new building options

---
+++ dist/precache-manifest.1a819a2d6939e3e84e9de8843beccc04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7bdf82446689e487456b",
+    "url": "/static/css/main.311c5297.chunk.css"
+  },
+  {
+    "revision": "7bdf82446689e487456b",
+    "url": "/static/js/main.7bdf8244.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "45d599a840ff1efe516f",
+    "url": "/static/js/2.45d599a8.chunk.js"
+  },
+  {
+    "revision": "8ba3075663cac21e8fc5",
+    "url": "/static/js/runtime~main.8ba30756.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "6694841c8e8462ebfea7bbeeac397aa7",
+    "url": "/index.html"
+  }
+];
